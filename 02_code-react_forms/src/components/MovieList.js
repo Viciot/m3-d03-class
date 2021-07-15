6ecee616163bd8aeb197f02a896cf443c65eeacb
Viciot <vfuentesnoguera@gmail.com>
2021-07-15 08:08:36 +0200

    class changes

--- 02_code-react_forms/src/components/MovieList.js
+++ 02_code-react_forms/src/components/MovieList.js
@@ -6,8 +6,20 @@ import AddMovie from "./AddMovie";
 import FilterMovies from "./FilterMovies";
 
 function MovieList() {
+
   const [moviesData, setMoviesData] = useState(moviesDataJSON);
   const [movies, setMovies] = useState(moviesDataJSON);
+
+
+  function addMovie(newMovie){
+    const updatedMovieData = [...moviesData, newMovie]
+    {/*this is equivalent to moviesData.concat([newMovie])
+    copy, modify and  */}
+    setMoviesData(updatedMovieData)
+  }
+
+
+
 
   const filterMovieList = (str) => {
     let filteredMovies;
@@ -25,6 +37,7 @@ function MovieList() {
   return (
     <div>
       <FilterMovies filterMovies={filterMovieList} />
+      <AddMovie addMovieHandler={addMovie}/>
       {movies.map((movie) => {
         return <MovieCard key={movie._id} movie={movie} />;
       })}
